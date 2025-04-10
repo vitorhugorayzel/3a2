5eae8f9008dbe1896f004ec4e5186adf04b5d195
vitorhugorayzel <143604653+vitorhugorayzel@users.noreply.github.com>
2025-04-10 15:10:04 -0300

    kkkkkk

--- main.js
+++ main.js
@@ -43,9 +43,9 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[7];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[4];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[9];
         document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
     }
 }
